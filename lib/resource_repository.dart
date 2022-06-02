aa153003cca5fe911c0a193e9b6c163862a3806d
Naik <naiksav4enko@gmail.com>
2022-06-02 03:37:40 +0300

    Fix exports

--- lib/resource_repository.dart
+++ lib/resource_repository.dart
@@ -4,3 +4,4 @@ export 'src/resource.dart';
 export 'src/stream_resource.dart';
 export 'src/stream_repository.dart';
 export 'src/memory_cache_storage.dart';
+export 'package:resource_repository_storage/resource_repository_storage.dart';
